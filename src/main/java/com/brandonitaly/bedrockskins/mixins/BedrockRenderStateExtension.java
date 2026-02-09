92f977ff0a5dd3c46b8c94e98660aa7b2f1d6c25
Brandon Flaherty <brandon.flaherty@outlook.com>
2026-02-09 15:18:38 -0800

    Remove BedrockRenderStateExtension interface as it is no longer needed

--- src/main/java/com/brandonitaly/bedrockskins/mixins/BedrockRenderStateExtension.java
+++
@@ -1,8 +0,0 @@
-package com.brandonitaly.bedrockskins.mixins;
-
-import java.util.UUID;
-
-public interface BedrockRenderStateExtension {
-    UUID getUniqueId();
-    void setUniqueId(UUID uuid);
-}
