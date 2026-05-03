252b63712e1e6874bf0ef258c56405757a45da94
Brandon Flaherty <brandon.flaherty@outlook.com>
2026-05-02 17:08:41 -0700

    refactor: update sprite identifiers to use legacy namespace and remove unused textures

--- src/main/java/com/brandonitaly/bedrockskins/util/BedrockSkinsSprites.java
+++ src/main/java/com/brandonitaly/bedrockskins/util/BedrockSkinsSprites.java
@@ -3,10 +3,6 @@ package com.brandonitaly.bedrockskins.util;
 import net.minecraft.resources.Identifier;
 
 public class BedrockSkinsSprites {
-    public static final Identifier SKIN_BOX = Identifier.fromNamespaceAndPath("bedrockskins", "tiles/skin_box");
-    public static final Identifier SKIN_PANEL = Identifier.fromNamespaceAndPath("bedrockskins", "tiles/skin_panel");
-    public static final Identifier PANEL_FILLER = Identifier.fromNamespaceAndPath("bedrockskins", "tiles/panel_filler");
-    public static final Identifier PACK_NAME_BOX = Identifier.fromNamespaceAndPath("bedrockskins", "tiles/pack_name_box");
     public static final Identifier PANEL_SPRITE = Identifier.fromNamespaceAndPath("bedrockskins", "container/skin_panel");
     public static final Identifier HANGAR_ICON = Identifier.fromNamespaceAndPath("bedrockskins", "container/icon_hangar");
     public static final Identifier ROTATE_SPRITE = Identifier.fromNamespaceAndPath("bedrockskins", "container/rotate");
@@ -16,6 +12,10 @@ public class BedrockSkinsSprites {
     public static final Identifier SKIN_DENY = Identifier.fromNamespaceAndPath("bedrockskins", "container/deny");
 
     // Legacy4J textures
+    public static final Identifier SKIN_BOX = Identifier.fromNamespaceAndPath("legacy", "tiles/skin_box");
+    public static final Identifier SKIN_PANEL = Identifier.fromNamespaceAndPath("legacy", "tiles/skin_panel");
+    public static final Identifier PANEL_FILLER = Identifier.fromNamespaceAndPath("legacy", "tiles/panel_filler");
+    public static final Identifier PACK_NAME_BOX = Identifier.fromNamespaceAndPath("legacy", "tiles/pack_name_box");
     public static final Identifier SQUARE_RECESSED_PANEL = Identifier.fromNamespaceAndPath("legacy", "tiles/square_recessed_panel");
     public static final Identifier ICON_HOLDER = Identifier.fromNamespaceAndPath("legacy", "container/icon_holder");
     public static final Identifier BEACON_CONFIRM = Identifier.fromNamespaceAndPath("legacy", "container/beacon_check");
